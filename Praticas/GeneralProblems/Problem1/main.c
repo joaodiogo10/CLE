00ae5ce6ce257da3631b9b25c7820ab99a6ca672
joaodiogo10 <joaodiogohf@ua.pt>
2022-03-19 11:48:58 +0000

    Problem 2

--- Praticas/GeneralProblems/Problem1/main.c
+++ Praticas/GeneralProblems/Problem1/main.c
@@ -20,8 +20,8 @@
 // Question mark (? 0x3F)
 // Exclamation point (! 0x21)
 // Dash (—, 0xE28093)
-// Ellipsis (…, 0xE280A6)
-const unsigned int delimiters[] = {0x20, 0x9, 0xA,                                          // Space character
+// Ellipsis (…, 0xE280A6) 
+const unsigned int delimiters[] = {0x20, 0x9, 0xA, 0xD,                                     // Space character
                                    0x2D, 0x22, 0xE2809C, 0xe2809D, 0x5D, 0x29,              // Separation symbol
                                    0x2E, 0x2C, 0x3A, 0x3B, 0x3F, 0x21, 0xE28093, 0xE280A6}; // Punctuation symbol
 
@@ -36,12 +36,13 @@ const unsigned int specialConsoants[] = {0xC3A7, 0xC387}; // c
 // u (0x75), ú (0xC3BA), ù (0xC3B9), U (0x55), Ú (0xC39A), Ù (0xC399)
 const unsigned int vowels[] = {0x61, 0xC3A1, 0xC3A0, 0xC3A2, 0xC3A3, 0x41, 0xC381, 0xC380, 0xC382, 0xC383, // a
                                0x65, 0xC3A9, 0xC3A8, 0xC3AA, 0x45, 0xC389, 0xC388, 0xC38A,                 // e
-                               0x69, 0xC3AD, 0xC3AC, 0xC38D, 0x49, 0xC38C,                                 // i
+                               0x69, 0xC3AD, 0xC3AC, 0x49, 0xC38D, 0xC38C,                                 // i
                                0x6F, 0xC3B3, 0xC3B2, 0xC3B4, 0xC3B5, 0x4F, 0xC393, 0xC392, 0xC394, 0xC395, // o
                                0x75, 0xC3BA, 0xC3B9, 0x55, 0xC39A, 0xC399};                                // u
 
-//apostrophe (' 0x27)
-const unsigned int apostrophe = 0x27;
+
+// underscore (_ 0x5F)
+const unsigned int underscore = 0x5F;
 
 #define ASCII_FIRST_UPPER_CASE_LETTER 0x41
 #define ASCII_LAST_UPPER_CASE_LETTER 0x5A
@@ -52,26 +53,27 @@ const unsigned int apostrophe = 0x27;
 enum CharacterType
 {
     CONSOANT,
+    UNDERSCORE,
     VOWEL,
     DELIMITER,
     EOFILE,
-    APOSTROPHE,
+    NOT_DEFINED,
     ERROR
 };
 
-enum CharacterType readUTF8Char(FILE *ptrFile);
+enum CharacterType readUTF8Char(FILE *ptrFile, unsigned int *utf8Char);
 
 int main(int argc, char *argv[])
 {
-    if(argc < 2)
+    if (argc < 2)
     {
         fprintf(stderr, "USAGE: ./countWords filePath [filePath ...]\n");
         return 1;
     }
 
-    for(int i = 1; i < argc; i++) //for each filePath
+    for (int i = 1; i < argc; i++) // for each filePath
     {
-        char* filePath = argv[i]; 
+        char *filePath = argv[i];
         FILE *ptrFile;
         ptrFile = fopen(filePath, "rb");
         if (ptrFile == NULL)
@@ -83,35 +85,105 @@ int main(int argc, char *argv[])
         unsigned int totalWords = 0;
         unsigned int totalWordsEndingInConsoant = 0;
         unsigned int totalWordsBeginningInVowel = 0;
-        enum CharacterType type = readUTF8Char(ptrFile);
-        enum CharacterType lastCharacterType = DELIMITER;
+        unsigned int utf8Char;
+        enum CharacterType type = readUTF8Char(ptrFile, &utf8Char);
+        enum CharacterType lastCharacterType = NOT_DEFINED;
 
-        bool mergeWord = false;
+        bool inWord = false;
 
         while (type != EOFILE)
         {
+            switch (inWord)
+            {
+            case false:
+                switch (type)
+                {
+                case CONSOANT:
+                    totalWords++;
+                    inWord = true;
+                    break;
+
+                case UNDERSCORE:
+                    totalWords++;
+                    inWord = true;
+
+                    break;
+
+                case VOWEL:
+                    totalWords++;
+                    totalWordsBeginningInVowel++;
+                    inWord = true;
+
+                    break;
+
+                case ERROR:
+                    fprintf(stderr, "ERROR reading character");
+                    break;
+
+                default:
+                    break;
+                }
+
+                break;
+
+            case true:
+                switch (type)
+                {
+                case DELIMITER:
+                    if(lastCharacterType == CONSOANT)
+                        totalWordsEndingInConsoant++;
+                    
+                    inWord = false;
+                    break;
+
+                case ERROR:
+                    fprintf(stderr, "ERROR reading character");
+                    break;
+                
+                default:
+                    break;
+                }
+                break;
+            }
+            /*
+            if(type == CONSOANT)
+            {
+                printf("Consoant\n");
+            }
+            if(type == VOWEL)
+            {
+                printf("Vowel\n");
+            }
+            if(type == DELIMITER)
+            {
+                printf("Delimiter\n");
+            }
+            if(type == UNDERSCORE)
+            {
+                printf("Underscore\n");
+            }*/
+            lastCharacterType = type;
+            type = readUTF8Char(ptrFile, &utf8Char);
+        }
+        /*while (type != EOFILE)
+        {
             switch (type)
             {
-            
+
             case APOSTROPHE:
-                if(mergeWord == true) //end merged word
-                    mergeWord = false;
-                else //beginning of merged word
-                    mergeWord = true;
-                
-                //lastCharacterType = lastCharacterType; keep lastCharacterType
-                break; 
-            
+                lastCharacterType = APOSTROPHE;
+                break;
+
             case VOWEL:
-                if ((mergeWord == false && lastCharacterType == DELIMITER))
+                if (lastCharacterType == DELIMITER)
                     totalWordsBeginningInVowel++;
-                
+
                 lastCharacterType = VOWEL;
                 break;
-            
+
             case DELIMITER:
                 //increment word count
-                if(mergeWord == false && lastCharacterType != DELIMITER)
+                if(lastCharacterType != DELIMITER)
                 {
                     if (lastCharacterType == CONSOANT) //check consoant
                         totalWordsEndingInConsoant++;
@@ -125,28 +197,32 @@ int main(int argc, char *argv[])
                 lastCharacterType = CONSOANT;
                 break;
 
+            case NOT_DEFINED:
+                lastCharacterType = NOT_DEFINED;
+                break;
+
             case ERROR:
                 fprintf(stderr, "Error reading character!\n");
                 break;
             }
 
-            type = readUTF8Char(ptrFile);
-        }
+            type = readUTF8Char(ptrFile, &utf8Char);
+        }*/
 
         fclose(ptrFile);
-        //print results
+        // print results
         fprintf(stdout,
-           "------File %s results------\n"
-           "Total words: %d\n"
-           "Total words beginning in vowel: %d\n"
-           "Total words ending in consoant: %d\n",
-           filePath, totalWords, totalWordsBeginningInVowel, totalWordsEndingInConsoant);
+                "------File %s results------\n"
+                "Total words: %d\n"
+                "Total words beginning in vowel: %d\n"
+                "Total words ending in consoant: %d\n\n",
+                filePath, totalWords, totalWordsBeginningInVowel, totalWordsEndingInConsoant);
     }
-    
+
     return 0;
 }
 
-enum CharacterType readUTF8Char(FILE *ptrFile)
+enum CharacterType readUTF8Char(FILE *ptrFile, unsigned int *utf8Char)
 {
     unsigned char buffer[3];
     fread(&buffer, sizeof(char), 1, ptrFile);
@@ -160,7 +236,7 @@ enum CharacterType readUTF8Char(FILE *ptrFile)
         return EOFILE;
 
     unsigned char tmp = buffer[0] & 0xF8;
-    unsigned int utf8Char = buffer[0];
+    *utf8Char = buffer[0];
 
     if (tmp >> 7 == 0) // 1 byte character
     {
@@ -174,7 +250,7 @@ enum CharacterType readUTF8Char(FILE *ptrFile)
         if (ferror(ptrFile) != 0 || feof(ptrFile) != 0)
             return ERROR;
 
-        utf8Char = (utf8Char << 8) | buffer[0];
+        *utf8Char = (*utf8Char << 8) | buffer[0];
     }
     else if (tmp >> 4 == 0b1110) // 3 bytes character
     {
@@ -184,7 +260,7 @@ enum CharacterType readUTF8Char(FILE *ptrFile)
         if (ferror(ptrFile) != 0 || feof(ptrFile) != 0)
             return ERROR;
 
-        utf8Char = (utf8Char << 16) | (buffer[0] << 8) | buffer[1];
+        *utf8Char = (*utf8Char << 16) | (buffer[0] << 8) | buffer[1];
     }
     else if (tmp >> 3 == 0b1110) // 4 bytes character
     {
@@ -194,7 +270,7 @@ enum CharacterType readUTF8Char(FILE *ptrFile)
         if (ferror(ptrFile) != 0 || feof(ptrFile) != 0)
             return ERROR;
 
-        utf8Char = (utf8Char << 24) | (buffer[0] << 16) | (buffer[1] << 8) | buffer[0];
+        *utf8Char = (*utf8Char << 24) | (buffer[0] << 16) | (buffer[1] << 8) | buffer[0];
     }
     // invalid
     else
@@ -205,34 +281,38 @@ enum CharacterType readUTF8Char(FILE *ptrFile)
     // Check if is a delimiter
     for (int i = 0; i < sizeof(delimiters) / sizeof(*delimiters); i++)
     {
-        if (delimiters[i] == utf8Char)
+        if (delimiters[i] == *utf8Char)
             return DELIMITER;
     }
-
-    // Check if is an apostrophe
-    if(apostrophe == utf8Char)
-        return APOSTROPHE;
 
     // Check if is a vowel
     for (int i = 0; i < sizeof(vowels) / sizeof(*vowels); i++)
     {
-        if (vowels[i] == utf8Char)
+        if (vowels[i] == *utf8Char)
             return VOWEL;
     }
 
     // Check if is a consoant
-    for (int i = 0; i < sizeof(specialConsoants) / sizeof(specialConsoants); i++)
+    for (int i = 0; i < sizeof(specialConsoants) / sizeof(*specialConsoants); i++)
     {
-        if (specialConsoants[i] == utf8Char)
+
+        if (specialConsoants[i] == *utf8Char)
         {
+
             return CONSOANT;
         }
     }
-    if ((ASCII_LAST_LOWER_CASE_LETTER >= utf8Char && utf8Char >= ASCII_FIRST_LOWER_CASE_LETTER) ||
-        (ASCII_LAST_UPPER_CASE_LETTER >= utf8Char && utf8Char >= ASCII_FIRST_UPPER_CASE_LETTER))
+    if ((ASCII_LAST_LOWER_CASE_LETTER >= *utf8Char && *utf8Char >= ASCII_FIRST_LOWER_CASE_LETTER) ||
+        (ASCII_LAST_UPPER_CASE_LETTER >= *utf8Char && *utf8Char >= ASCII_FIRST_UPPER_CASE_LETTER))
     {
         return CONSOANT;
     }
 
-    return ERROR;
+    // Check if is an underscore
+    if (*utf8Char == underscore)
+    {
+        return UNDERSCORE;
+    }
+
+    return NOT_DEFINED;
 }
---
+++ Praticas/GeneralProblems/Problem2/main.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+char * filePath = "dataSetProb2b/computeDet/mat128_32.txt";
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "USAGE: ./matrixDeterminant filePath\n");
+        return EXIT_FAILURE;
+    }
+
+    char *filePath = argv[1];
+    FILE *ptrFile;
+    ptrFile = fopen(filePath, "rb");
+    if (ptrFile == NULL)
+    {
+        fprintf(stderr, "Error opening file \"%s\"\n", filePath);
+        return EXIT_FAILURE;
+    }
+    
+    unsigned int nMatrices;
+    fread(&nMatrices, sizeof(unsigned int), 1, ptrFile);
+    if (ferror(ptrFile) != 0 || feof(ptrFile))
+    {
+        fprintf(stderr, "Error: invalid file format\n");
+        return EXIT_FAILURE;
+    }
+
+    unsigned int order;
+    fread(&order, sizeof(unsigned int), 1, ptrFile);
+    if (ferror(ptrFile) != 0 || feof(ptrFile))
+    {
+        fprintf(stderr, "Error: invalid file format\n");
+        return 1;
+    }
+
+    //start
+    printf("Computing %d matrix (%d,%d)...\n", nMatrices, order, order);
+    double m[order][order];
+
+    //parse matrix
+    for(int row = 0; row < order; row++)
+    {
+        for(int col = 0; col < order; col++)
+        {
+            char bytes[8];
+            fread(&bytes, 8, 1, ptrFile);
+            if (ferror(ptrFile) != 0 || feof(ptrFile))
+            {
+                fprintf(stderr, "Error parsing matrix entries, verify file format\n");
+                return EXIT_FAILURE;
+            }
+            double a = *((double*)bytes);
+            m[row][col] = a;
+        }
+    }
+
+    //print matrix
+    /*for(int row = 0; row < order; row++)
+    {
+        printf("\n%d- ", row);
+        for(int col = 0; col < order; col++)
+        {
+            printf("%f ", m[row][col]);
+        }
+    }*/
+
+    for(int matrix = 0; matrix < nMatrices ; matrix++) //for each matrix
+    { 
+        printf("\n----Computing matrix %d----\n", matrix + 1);
+        double determinant = 1;
+        unsigned int nSwap = 0;
+
+        //perform gausian elimination
+        for(int i = 0; i < order - 1; i++) 
+        {
+            if(m[i][i] == 0) 
+            {  
+                bool swapped = false; 
+                //find a col, j, whose coefficient m[i][j] != 0, for j > i. Then swap cols
+                for(int j = i+1; j < order; i++) 
+                {
+                    if(m[i][j] != 0)
+                    {
+                        //swap cols
+                        for(int row = 0; row < order; row++)
+                        {
+                            double tmp = m[row][i]; //col i
+                            m[row][i] = m[row][j];  //col i = col j
+                            m[row][j] = tmp;        //col j = col i
+                        }
+                        swapped = true;
+                        nSwap += 1;
+                        break;
+                    }
+                }
+
+                //all col's coefficients == 0 => determinant = 0 
+                if(swapped == false)
+                {
+                    determinant = 0;
+                    break;
+                }
+            }
+            //apply transformation a_kj = a_kj - a_ki/a_ii * a_ij
+            for(int k = i + 1; k < order; k++) //for all rows, k, below row i
+            {   
+                for(int j = i; j < order; j++) //for all cols, starting at col i (diagonal)
+                {
+                    m[k][j] = m[k][j] - m[k][i]/m[i][i] * m[i][j];
+                }
+            }
+            //printf("%e\n", m[i][i]);
+            //multiple diagonal
+            determinant = determinant * m[i][i];
+        }
+
+        //determine determinant signal
+        determinant = -determinant ? nSwap % 2 == 1 : determinant;
+
+        printf("Determinant: %e\n", determinant);
+    }
+    return EXIT_SUCCESS;
+}
